beafceec90a4db8769cb8fec5ee5372f3c7c7fc4
Ghanshyam <you@example.com>
2024-07-13 21:41:47 +0530

    cleaned some old comments

--- src/main/java/dev/ghanshyam/productservicev2/repositories/ProductServiceRepo.java
+++ src/main/java/dev/ghanshyam/productservicev2/repositories/ProductServiceRepo.java
@@ -20,14 +20,10 @@ public interface ProductServiceRepo extends JpaRepository<Product, UUID> {
     @Query("select MAX(p.product_id) from Product p")
     public Long getLastProductId();
 
-    //public List<Product> getProductsByCategory_Category(String category);
     @Query("select p from Product p JOIN Category c ON p.category=c where c.category=:cat_name")
     public List<Product> getProductsByCategory_Category(String cat_name);
 
     public Product save(Product p);
-
-//    @Query("delete from Product p where p.product_id=:id")
-//    public void deleteProductBy(Long id);
 
     public void delete(Product product);
 }
